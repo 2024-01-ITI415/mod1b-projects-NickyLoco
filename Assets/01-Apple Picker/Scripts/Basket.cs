396684674436249ab83737177a72cc1338829dba
NickyLoco <157553478+NickyLoco@users.noreply.github.com>
2024-02-05 23:44:53 -0500

    NewBuild - with score

--- Assets/01-Apple Picker/Scripts/Basket.cs
+++ Assets/01-Apple Picker/Scripts/Basket.cs
@@ -1,13 +1,21 @@
-using	System.Collections.Generic;
+using	System.Collections;
+ using	System.Collections.Generic;
  using	UnityEngine;
- using	UnityEngine.UI;	//	This	line	enables	use	of	uGUI	features.	//	a
+using UnityEngine.UI;
+
  public	class	Basket	:	MonoBehaviour	{
- [Header("Set	Dynamically")]
+[Header("Set Dynamically")]
 public	Text	scoreGT;	//	a
  void	Start()	{
+ //	Find	a	reference	to	the	ScoreCounter	GameObject
+ GameObject	scoreGO	=	GameObject.Find("ScoreCounter");	//	b
+ //	Get	the	Text	Component	of	that	GameObject
+ scoreGT	=	scoreGO.GetComponent<Text>();	//	c
+ //	Set	the	starting	number	of	points	to	0
+ scoreGT.text	=	"0";
+ }
 
- } 
- void	Update()	{
+ void	Update	()	{
  //	Get	the	current	screen	position	of	the	mouse	from	Input
  Vector3	mousePos2D	=	Input.mousePosition;	//	a
  //	The	Camera's	z	position	sets	how	far	to	push	the	mouse	into	3D
@@ -19,11 +27,24 @@ public	Text	scoreGT;	//	a
  pos.x	=	mousePos3D.x;
  this.transform.position	=	pos;
  }
-void OnCollisionEnter(Collision	coll)	{	
-//	Find	out	what	hit	this	basket
- GameObject	collidedWith	=	coll.gameObject;	
-if (collidedWith.tag	==	"Apple")	{	
-Destroy(collidedWith);	
+
+ void	OnCollisionEnter(	Collision	coll	)	{	//	a
+ //	Find	out	what	hit	this	basket
+ GameObject	collidedWith	=	coll.gameObject;	//	b
+ if	(	collidedWith.tag	==	"Apple"	)	{	//	c
+ Destroy(	collidedWith	);
+
+ //	Parse	the	text	of	the	scoreGT	into	an	int
+ int	score	=	int.Parse(	scoreGT.text	);	//	d
+ //	Add	points	for	catching	the	apple
+ score	+=	100;
+ //	Convert	the	score	back	to	a	string	and	display	it
+ scoreGT.text	=	score.ToString();
+
+ //	Track	the	high	score
+ if	(score	>	HighScore.score)	{
+ HighScore.score	=	score;
+ }
  }
  }
  }
--- Assets/01-Apple Picker/Scripts/HighScore.cs
+++ Assets/01-Apple Picker/Scripts/HighScore.cs
@@ -3,32 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HighScore : MonoBehaviour
-{
-    static public int score = 1000;
-    void Awake()
-{
-    // If the player high score already exists then read it
-    if (PlayerPrefs.HasKey("HighScore"))
-    {
-        // Read the existing high score from PlayerPrefs
-        int savedHighScore = PlayerPrefs.GetInt("HighScore");
-
-        // Set the static score variable to the saved high score
-        score = savedHighScore;
-    }
-    else
-    {
-        // If there is no saved high score, set the PlayerPrefs to the default score
-        PlayerPrefs.SetInt("HighScore", score);
-    }
-}
-    void Update() {
-    Text gt	= this.GetComponent<Text>();
-    gt.text	= "High Score: "+score;
-    // Update	the	PlayerPrefs	HighScore	if	necessary
-    if (score > PlayerPrefs.GetInt("HighScore")) {
-    PlayerPrefs.SetInt("HighScore",	score);
-  }
-  }
+public	class	HighScore	:	MonoBehaviour	{
+static	public	int	score	=	1000;
+ void	Awake()	{	//	a
+ //	If	the	PlayerPrefs	HighScore	already	exists,	read	it
+ if	(PlayerPrefs.HasKey("HighScore"))	{	//	b
+ score	=	PlayerPrefs.GetInt("HighScore");
+ }
+ //	Assign	the	high	score	to	HighScore
+ PlayerPrefs.SetInt("HighScore", score);	//	c
+ }
+ void	Update	()	{
+ Text	gt	=	this.GetComponent<Text>();
+ gt.text	=	"High Score: "+score;
+ //	Update	the	PlayerPrefs	HighScore	if	necessary
+ if	(score	> PlayerPrefs.GetInt("HighScore"))	{	//	d
+ PlayerPrefs.SetInt("HighScore", score);
+ }
+ }
 }
